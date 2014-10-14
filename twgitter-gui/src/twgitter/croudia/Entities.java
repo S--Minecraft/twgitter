c67388a4c01489f31e42b669e122fbfebf78cf6d
S <dainihon.koukoku@gmail.com>
2014-10-14 21:05:12 +0900

    json change package

--- twgitter-gui/src/twgitter/croudia/Entities.java
+++
@@ -1,7 +0,0 @@
-package twgitter.croudia;
-
-public class Entities {
-	Media media;
-
-	public Media getMedia(){return media;}
-}
--- twgitter-gui/src/twgitter/croudia/Media.java
+++
@@ -1,9 +0,0 @@
-package twgitter.croudia;
-
-public class Media {
-	String media_url_https;
-	String type;
-
-	public String getMedia_url_https(){return media_url_https;}
-	public String getTyoe(){return type;}
-}
